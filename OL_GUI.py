e0b00c0f1a8028bebe8e126dfb749dffaf1fe634
ItsJustRey <russellrubio96@gmail.com>
2018-03-03 14:30:02 -0500

    Added 3D OpenGL graph. Axis/View needs to be adjusted.

--- OL_GUI.py
+++ OL_GUI.py
@@ -31,7 +31,6 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from mpl_toolkits import mplot3d
 import Object_Localization
 from collections import deque
-
 import random
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -40,57 +39,102 @@ class OL_GUI(QDialog):
     def __init__(self):
         super(OL_GUI, self).__init__()
 
-        loadUi('GUI_test.ui', self)
+        loadUi('GUI.ui', self)
+        self.showMaximized()
 
         ap = argparse.ArgumentParser()
-        ap.add_argument("-v", "--video", help="path to the (optional) video file")
         ap.add_argument("-b", "--buffer", type=int, default=128, help="max buffer size")
         self.args = vars(ap.parse_args())
-
-        self.v0_clear()
-
-
-
         self.image = None
+
+        self.clear()
         self.button_start.clicked.connect(self.start_video)
         self.button_stop.clicked.connect(self.stop_video)
-        self.setup_plot()
+        self.button_clear.clicked.connect(self.clear)
+
+        self.plot = OL_3D_Plot(self)
+        self.layout_plot.addWidget(self.plot)
 
 
-
-
+    # INITIALIZE VIDEO 0, VIDEO 1, and VIDEO 2 FRAMES AND DATA
     def start_video(self):
         print("start")
+
+        # INITIALIZE VIDEO 0 FRAMES AND DATA
         self.video0 = cv2.VideoCapture(0)
         self.video0.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
         self.video0.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+        #self.v0_xArray = self.v0_yArray = self.v0_zArray = []
+        #self.v0_x = self.v0_y = self.v0_z = None
 
+        # INITIALIZE VIDEO 1 FRAMES AND DATA
+        # self.video1 = cv2.VideoCapture(0)
+        # self.video1.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
+        # self.video1.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+        # self.v1_xArray = self.v1_yArray = self.v1_zArray = []
+        # self.v1_x = self.v1_y = self.v1_z = None
+
+        # INITIALIZE VIDEO 2 FRAMES AND DATA
+        # self.video2 = cv2.VideoCapture(0)
+        # self.video2.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
+        # self.video2.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+        # self.v2_xArray = self.v2_yArray = self.v2_zArray = []
+        # self.v2_x = self.v2_y = self.v2_z = None
+
+
+        # CREATE TIMER THREAD TO UPDATE FRAME EVERY (x) milliseconds
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(1)
 
-
-
-
-        #self.plot_data()
-
+    # GET VIDEO 0, VIDEO 1, and VIDEO 2 FRAMES AND DATA
     def update_frame(self):
-        ret, self.frame = self.video0.read()
+        ret, self.v0_frame = self.video0.read()
+        #ret, self.v1_frame = self.video1.read()
+        #ret, self.v2_frame = self.video2.read()
 
-        (self.frame, self.v0_x, self.v0_y, self.v0_z, self.pts, self.counter, self.isDetected) = \
-            Object_Localization.Object_Localization(self.frame, self.pts, self.counter)
+        # VIDEO 0 OBJECT DETECTION returns data of given frame
+        (self.v0_frame, self.v0_x, self.v0_y, self.v0_z, self.v0_pts, self.v0_counter, self.v0_isDetected) = \
+            Object_Localization.Object_Localization(self.v0_frame, self.v0_pts, self.v0_counter)
 
-        if (self.isDetected):
-            if (len(self.pts) > 10):
+        # VIDEO 1 OBJECT DETECTION returns data of given frame
+        #(self.v1_frame, self.v1_x, self.v1_y, self.v1_z, self.v1_pts, self.v1_counter, self.v1_isDetected) = \
+        #    Object_Localization.Object_Localization(self.v0_frame, self.v1_pts, self.v1_counter)
+
+        # VIDEO 2 OBJECT DETECTION returns data of given frame
+        #(self.v2_frame, self.v2_x, self.v2_y, self.v2_z, self.v2_pts, self.v2_counter, self.v2_isDetected) = \
+        #    Object_Localization.Object_Localization(self.v2_frame, self.v2_pts, self.v2_counter)
+
+        if (self.v0_isDetected):
+            if (len(self.v0_pts) > 10):
                 self.v0_xArray.append(self.v0_x)
                 self.v0_yArray.append(self.v0_y)
                 self.v0_zArray.append(self.v0_z)
 
-        else:
-            self.v0_clear()
+            # if (len(self.v1_pts) > 10):
+            #     self.v1_xArray.append(self.v1_x)
+            #     self.v1_yArray.append(self.v1_y)
+            #     self.v1_zArray.append(self.v1_z)
 
-        self.frame = cv2.flip(self.frame, 1)
-        self.display_frame(self.frame, 1)
+            # if (len(self.v2_pts) > 10):
+            #     self.v2_xArray.append(self.v2_x)
+            #     self.v2_yArray.append(self.v2_y)
+            #     self.v2_zArray.append(self.v2_z)
+
+        else:
+            self.clear()
+
+
+        # USE NUMPY TO COMBINE v0
+        self.v0_xyz = np.vstack([self.v0_xArray, self.v0_yArray, self.v0_zArray]).transpose()
+        #self.v1_xyz = np.vstack([self.v1_xArray, self.v1_yArray, self.v1_zArray]).transpose()
+        #self.v2_xyz = np.vstack([self.v2_xArray, self.v2_yArray, self.v2_zArray]).transpose()
+        self.plot.traces.setData(pos = self.v0_xyz)
+
+
+        self.v0_frame = cv2.flip(self.v0_frame, 1)
+        self.display_frame(self.v0_frame, 1)
+
 
     def display_frame(self, _frame, window = 1):
         qformat = QImage.Format_Indexed8
@@ -102,6 +146,8 @@ class OL_GUI(QDialog):
 
         outputImage = QImage(_frame, _frame.shape[1], _frame.shape[0], _frame.strides[0], qformat)
         outputImage = outputImage.rgbSwapped()
+
+
 
         if window == 1:
             self.label_video0.setPixmap(QPixmap.fromImage(outputImage))
@@ -115,107 +161,75 @@ class OL_GUI(QDialog):
         print("stop")
         self.timer.stop()
 
-    def v0_clear(self):
-        self.v0_xArray = []
-        self.v0_yArray = []
-        self.v0_zArray = []
-        self.v0_x = None
-        self.v0_y = None
-        self.v0_z = None
-        self.counter = 0
-        self.pts = deque(maxlen=self.args["buffer"])
+
+    # USED TO CLEAR VIDEO 0, VIDEO 1, VIDEO 2 DATA STRUCTURES
+    def clear(self):
+
+        # CLEAR VIDEO 0 DATA STRUCTURES
+        self.v0_xArray = []; self.v0_yArray = []; self.v0_zArray = []
+        self.v0_x = None; self.v0_y = None; self.v0_z = None
+        self.v0_counter = 0
+        self.v0_pts = deque(maxlen=self.args["buffer"])
+
+        # CLEAR VIDEO 1 DATA STRUCTURES
+        # self.v1_xArray = self.v1_yArray = self.v1_zArray = []
+        # self.v1_x = self.v1_y = self.v1_z = None
+        # self.v1_counter = 0
+        # self.v1_pts = deque(maxlen=self.args["buffer"])
+
+        # CLEAR VIDEO 2 DATA STRUCTURES
+        # self.v2_xArray = self.v2_yArray = self.v2_zArray = []
+        # self.v2_x = self.v2_y = self.v2_z = None
+        # self.v2_counter = 0
+        # self.v2_pts = deque(maxlen=self.args["buffer"])
+
         return
 
+# OPENGL 3D PLOT WIDGET
+class OL_3D_Plot(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(OL_3D_Plot, self).__init__(parent)
 
-    def setup_plot(self):
+        layout = QtGui.QHBoxLayout()
 
-        w = gl.GLViewWidget()
-        w.opts['distance'] = 40
-        # w.show()
-        w.setWindowTitle('pyqtgraph example: GLLinePlotItem')
+        self.plot = gl.GLViewWidget()
+        self.plot.opts['distance'] = 2000
+        self.plot.setWindowTitle('3-Dimensional Plot')
+        self.plot.opts['fov'] = 1
+        #self.plot.setGeometry(0, 110, 1920, 1080)
+        #self.plot.show()
 
-        gx = gl.GLGridItem()
+        # create the background grids
+        gx = gl.GLGridItem(color = "b")
         gx.rotate(90, 0, 1, 0)
         gx.translate(-10, 0, 0)
-        w.addItem(gx)
+
         gy = gl.GLGridItem()
         gy.rotate(90, 1, 0, 0)
         gy.translate(0, -10, 0)
-        w.addItem(gy)
+
         gz = gl.GLGridItem()
         gz.translate(0, 0, -10)
-        w.addItem(gz)
 
-        def fn(x, y):
-            return np.cos((x ** 2 + y ** 2) ** 0.5)
+        #gx.scale(1, 0.1, 0.1)
+        #gy.scale(0.2, 0.1, 0.1)
+        #gz.scale(0.1, 0.2, 0.1)
 
-        n = 51
-        y = np.linspace(-10, 10, n)
-        x = np.linspace(-10, 10, 100)
-        for i in range(n):
-            yi = np.array([y[i]] * 100)
-            d = (x ** 2 + yi ** 2) ** 0.5
-            z = 10 * np.cos(d) / (d + 1)
-            pts = np.vstack([x, yi, z]).transpose()
-            plt = gl.GLLinePlotItem(pos=pts, color=pg.glColor((i, n * 1.3)), width=(i + 1) / 10., antialias=True)
-            w.addItem(plt)
-        self.layout_plot.addWidget(w)
+        self.plot.addItem(gx)
+        self.plot.addItem(gy)
+        self.plot.addItem(gz)
+        self.plot.showMaximized()
+        #pts = np.vstack(0, 0 ,0).transpose()
+        #traces= gl.GLLinePlotItem(pos=pts)
+        #self.plot.addItem(traces)
+        v0_xArray = []
+        v0_yArray = []
+        v0_zArray = []
+        #self.plot.resizeGL(400,400)
+        plot_xyz = np.vstack([v0_xArray, v0_yArray, v0_zArray]).transpose()
+        # self.curve = self.plot.plot.getPlotItem().plot()
+        self.traces = gl.GLLinePlotItem(pos=plot_xyz)
+        self.plot.addItem(self.traces)
 
-class PlotCanvas(FigureCanvas):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                                   QSizePolicy.Expanding,
-                                   QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-        self.plot()
-
-    def plot(self):
-        data = [random.random() for i in range(25)]
-        ax = self.figure.add_subplot(111)
-        ax.plot(data, 'r-')
-        ax.set_title('PyQt Matplotlib Example')
-        self.draw()
-
-
-class DynamicPlotter():
-
-    def __init__(self, sampleinterval=0.1, timewindow=10.):
-        # Data stuff
-        self._interval = int(sampleinterval*1000)
-        self._bufsize = int(timewindow/sampleinterval)
-        self.databuffer = collections.deque([0.0]*self._bufsize, self._bufsize)
-        self.x = np.linspace(-timewindow, 0.0, self._bufsize)
-        self.y = np.zeros(self._bufsize, dtype=np.float)
-        # PyQtGraph stuff
-        self.app = QtGui.QApplication([])
-        self.plt = pg.plot(title='Dynamic Plotting with PyQtGraph')
-        self.plt.showGrid(x=True, y=True)
-        self.plt.setLabel('left', 'amplitude', 'V')
-        self.plt.setLabel('bottom', 'time', 's')
-        self.curve = self.plt.plot(self.x, self.y, pen=(255,0,0))
-        # QTimer
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updateplot)
-        self.timer.start(self._interval)
-
-    def getdata(self):
-        frequency = 0.5
-        noise = random.normalvariate(0., 1.)
-        new = 10.*math.sin(time.time()*frequency*2*math.pi) + noise
-        return new
-
-    def updateplot(self):
-        self.databuffer.append( self.getdata() )
-        self.y[:] = self.databuffer
-        self.curve.setData(self.x, self.y)
-        self.app.processEvents()
-
-    def run(self):
-        self.app.exec_()
+        layout.addWidget(self.plot)
+        self.setLayout(layout)
