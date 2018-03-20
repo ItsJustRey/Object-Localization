153b5c46dfe408c60b8edb06f437d763126cdf45
rey-rubio <reynerio.r.rubio@gmail.com>
2018-03-20 13:59:13 -0400

    Multiple ball color tracking and plotting

--- OL_GUI.py
+++ OL_GUI.py
@@ -1,3 +1,4 @@
+
 import Object_Localization as Object_Localization
 
 #
@@ -9,19 +10,19 @@ from tkinter import *
 import argparse
 import cv2
 import matplotlib
-#matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 from PyQt5.uic.properties import QtWidgets, QtCore
 
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
 from pyqtgraph.Qt import QtGui, QtCore
 
-
 import collections
 import random
 import time
 import math
 import numpy as np
+
 matplotlib.use('QT5Agg')
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -34,6 +35,7 @@ from collections import deque
 import random
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+
 
 class OL_GUI(QDialog):
     def __init__(self):
@@ -53,8 +55,13 @@ class OL_GUI(QDialog):
         self.button_clear.clicked.connect(self.clear)
 
         self.plot = OL_3D_Plot(self)
+        self.plot1 = OL_3D_Plot(self)
+        self.plot2 = OL_3D_Plot(self)
+        self.plot3 = OL_3D_Plot(self)
         self.layout_plot.addWidget(self.plot)
-
+        self.layout_plot.addWidget(self.plot1)
+        self.layout_plot.addWidget(self.plot2)
+        self.layout_plot.addWidget(self.plot3)
 
     # INITIALIZE VIDEO 0, VIDEO 1, and VIDEO 2 FRAMES AND DATA
     def start_video(self):
@@ -64,23 +71,6 @@ class OL_GUI(QDialog):
         self.video0 = cv2.VideoCapture(0)
         self.video0.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
         self.video0.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
-        #self.v0_xArray = self.v0_yArray = self.v0_zArray = []
-        #self.v0_x = self.v0_y = self.v0_z = None
-
-        # INITIALIZE VIDEO 1 FRAMES AND DATA
-        # self.video1 = cv2.VideoCapture(0)
-        # self.video1.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-        # self.video1.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
-        # self.v1_xArray = self.v1_yArray = self.v1_zArray = []
-        # self.v1_x = self.v1_y = self.v1_z = None
-
-        # INITIALIZE VIDEO 2 FRAMES AND DATA
-        # self.video2 = cv2.VideoCapture(0)
-        # self.video2.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-        # self.video2.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
-        # self.v2_xArray = self.v2_yArray = self.v2_zArray = []
-        # self.v2_x = self.v2_y = self.v2_z = None
-
 
         # CREATE TIMER THREAD TO UPDATE FRAME EVERY (x) milliseconds
         self.timer = QTimer(self)
@@ -90,53 +80,72 @@ class OL_GUI(QDialog):
     # GET VIDEO 0, VIDEO 1, and VIDEO 2 FRAMES AND DATA
     def update_frame(self):
         ret, self.v0_frame = self.video0.read()
-        #ret, self.v1_frame = self.video1.read()
-        #ret, self.v2_frame = self.video2.read()
+        # ret, self.v1_frame = self.video1.read()
+        # ret, self.v2_frame = self.video2.read()
 
         # VIDEO 0 OBJECT DETECTION returns data of given frame
-        (self.v0_frame, self.v0_x, self.v0_y, self.v0_z, self.v0_pts, self.v0_counter, self.v0_isDetected) = \
+        (self.v0_frame,
+         self.v0_x_red, self.v0_y_red, self.v0_z_red, \
+         self.v0_x_green, self.v0_y_green, self.v0_z_green, \
+         self.v0_x_blue, self.v0_y_blue, self.v0_z_blue, \
+         self.v0_x_yellow, self.v0_y_yellow, self.v0_z_yellow,
+         self.v0_pts, self.v0_counter, self.v0_isRedDetected, self.v0_isGreenDetected, self.v0_isBlueDetected, self.v0_isYellowDetected) = \
             Object_Localization.Object_Localization(self.v0_frame, self.v0_pts, self.v0_counter)
 
         # VIDEO 1 OBJECT DETECTION returns data of given frame
-        #(self.v1_frame, self.v1_x, self.v1_y, self.v1_z, self.v1_pts, self.v1_counter, self.v1_isDetected) = \
+        # (self.v1_frame, self.v1_x, self.v1_y, self.v1_z, self.v1_pts, self.v1_counter, self.v1_isDetected) = \
         #    Object_Localization.Object_Localization(self.v0_frame, self.v1_pts, self.v1_counter)
 
         # VIDEO 2 OBJECT DETECTION returns data of given frame
-        #(self.v2_frame, self.v2_x, self.v2_y, self.v2_z, self.v2_pts, self.v2_counter, self.v2_isDetected) = \
+        # (self.v2_frame, self.v2_x, self.v2_y, self.v2_z, self.v2_pts, self.v2_counter, self.v2_isDetected) = \
         #    Object_Localization.Object_Localization(self.v2_frame, self.v2_pts, self.v2_counter)
 
-        if (self.v0_isDetected):
+        # if(self.v0_isRedDetected | self.v0_isGreenDetected | self.v0_isBlueDetected | self.v0_isYellowDetected):
+        if (self.v0_isRedDetected):
             if (len(self.v0_pts) > 10):
-                self.v0_xArray.append(self.v0_x)
-                self.v0_yArray.append(self.v0_y)
-                self.v0_zArray.append(self.v0_z)
+                self.v0_red_xArray.append(self.v0_x_red)
+                self.v0_red_yArray.append(self.v0_y_red)
+                self.v0_red_zArray.append(self.v0_z_red)
 
-            # if (len(self.v1_pts) > 10):
-            #     self.v1_xArray.append(self.v1_x)
-            #     self.v1_yArray.append(self.v1_y)
-            #     self.v1_zArray.append(self.v1_z)
+        elif (self.v0_isGreenDetected):
+          if (len(self.v0_pts) > 10):
+                self.v0_green_xArray.append(self.v0_x_green)
+                self.v0_green_yArray.append(self.v0_y_green)
+                self.v0_green_zArray.append(self.v0_z_green)
 
-            # if (len(self.v2_pts) > 10):
-            #     self.v2_xArray.append(self.v2_x)
-            #     self.v2_yArray.append(self.v2_y)
-            #     self.v2_zArray.append(self.v2_z)
+        elif (self.v0_isBlueDetected):
+            if (len(self.v0_pts) > 10):
+                self.v0_blue_xArray.append(self.v0_x_blue)
+                self.v0_blue_yArray.append(self.v0_y_blue)
+                self.v0_blue_zArray.append(self.v0_z_blue)
+
+        elif (self.v0_isYellowDetected):
+            if (len(self.v0_pts) > 10):
+                self.v0_yellow_xArray.append(self.v0_x_yellow)
+                self.v0_yellow_yArray.append(self.v0_y_yellow)
+                self.v0_yellow_zArray.append(self.v0_z_yellow)
 
         else:
             self.clear()
 
-
         # USE NUMPY TO COMBINE v0
-        self.v0_xyz = np.vstack([self.v0_xArray, self.v0_yArray, self.v0_zArray]).transpose()
-        #self.v1_xyz = np.vstack([self.v1_xArray, self.v1_yArray, self.v1_zArray]).transpose()
-        #self.v2_xyz = np.vstack([self.v2_xArray, self.v2_yArray, self.v2_zArray]).transpose()
-        self.plot.traces.setData(pos = self.v0_xyz)
+        self.v0_red_xyz = np.vstack([self.v0_red_xArray, self.v0_red_yArray, self.v0_red_zArray]).transpose()
+        self.v0_green_xyz = np.vstack([self.v0_green_xArray, self.v0_green_yArray, self.v0_green_zArray]).transpose()
+        self.v0_blue_xyz = np.vstack([self.v0_blue_xArray, self.v0_blue_yArray, self.v0_blue_zArray]).transpose()
+        self.v0_yellow_xyz = np.vstack([self.v0_yellow_xArray, self.v0_yellow_yArray, self.v0_yellow_zArray]).transpose()
 
+
+        # self.v1_xyz = np.vstack([self.v1_xArray, self.v1_yArray, self.v1_zArray]).transpose()
+        # self.v2_xyz = np.vstack([self.v2_xArray, self.v2_yArray, self.v2_zArray]).transpose()
+        self.plot.traces.setData(pos=self.v0_red_xyz)
+        self.plot1.traces.setData(pos=self.v0_green_xyz)
+        self.plot2.traces.setData(pos=self.v0_blue_xyz)
+        self.plot3.traces.setData(pos=self.v0_yellow_xyz)
 
         self.v0_frame = cv2.flip(self.v0_frame, 1)
         self.display_frame(self.v0_frame, 1)
 
-
-    def display_frame(self, _frame, window = 1):
+    def display_frame(self, _frame, window=1):
         qformat = QImage.Format_Indexed8
         if len(_frame.shape) == 3:
             if _frame.shape[2] == 4:
@@ -146,8 +155,6 @@ class OL_GUI(QDialog):
 
         outputImage = QImage(_frame, _frame.shape[1], _frame.shape[0], _frame.strides[0], qformat)
         outputImage = outputImage.rgbSwapped()
-
-
 
         if window == 1:
             self.label_video0.setPixmap(QPixmap.fromImage(outputImage))
@@ -161,13 +168,26 @@ class OL_GUI(QDialog):
         print("stop")
         self.timer.stop()
 
-
     # USED TO CLEAR VIDEO 0, VIDEO 1, VIDEO 2 DATA STRUCTURES
     def clear(self):
-
+        print("clearing");
         # CLEAR VIDEO 0 DATA STRUCTURES
-        self.v0_xArray = []; self.v0_yArray = []; self.v0_zArray = []
-        self.v0_x = None; self.v0_y = None; self.v0_z = None
+        self.v0_red_xArray = []
+        self.v0_red_yArray = []
+        self.v0_red_zArray = []
+        self.v0_green_xArray = []
+        self.v0_green_yArray = []
+        self.v0_green_zArray = []
+        self.v0_blue_xArray = []
+        self.v0_blue_yArray = []
+        self.v0_blue_zArray = []
+        self.v0_yellow_xArray = []
+        self.v0_yellow_yArray = []
+        self.v0_yellow_zArray = []
+        self.v0_x_red = None; self.v0_y_red = None; self.v0_z_red = None;
+        self.v0_x_green = None; self.v0_y_green = None; self.v0_z_green = None;
+        self.v0_x_blue = None; self.v0_y_blue = None; self.v0_z_blue = None;
+        self.v0_x_yellow = None; self.v0_y_yellow = None; self.v0_z_yellow = None;
         self.v0_counter = 0
         self.v0_pts = deque(maxlen=self.args["buffer"])
 
@@ -185,6 +205,7 @@ class OL_GUI(QDialog):
 
         return
 
+
 # OPENGL 3D PLOT WIDGET
 class OL_3D_Plot(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -195,12 +216,17 @@ class OL_3D_Plot(QtGui.QWidget):
         self.plot = gl.GLViewWidget()
         self.plot.opts['distance'] = 500
         self.plot.setWindowTitle('3-Dimensional Plot')
-        #self.plot.opts['fov'] = 1
-        #self.plot.setGeometry(0, 110, 1920, 1080)
-        #self.plot.show()
+
+        # self.plot1 = gl.GLViewWidget()
+        # self.plot1.opts['distance'] = 500
+        # self.plot1.setWindowTitle('3-Dimensional Plot')
+
+        # self.plot.opts['fov'] = 1
+        # self.plot.setGeometry(0, 110, 1920, 1080)
+        # self.plot.show()
 
         # create the background grids
-        gx = gl.GLGridItem(color = "b")
+        gx = gl.GLGridItem(color="b")
         gx.rotate(90, 0, 1, 0)
         gx.translate(-10, 0, 0)
 
@@ -211,21 +237,24 @@ class OL_3D_Plot(QtGui.QWidget):
         gz = gl.GLGridItem()
         gz.translate(0, 0, -10)
 
-        #gx.scale(1, 0.1, 0.1)
-        #gy.scale(0.2, 0.1, 0.1)
-        #gz.scale(0.1, 0.2, 0.1)
+        # gx.scale(1, 0.1, 0.1)
+        # gy.scale(0.2, 0.1, 0.1)
+        # gz.scale(0.1, 0.2, 0.1)
 
         self.plot.addItem(gx)
         self.plot.addItem(gy)
         self.plot.addItem(gz)
-        self.plot.showMaximized()
-        #pts = np.vstack(0, 0 ,0).transpose()
-        #traces= gl.GLLinePlotItem(pos=pts)
-        #self.plot.addItem(traces)
+        # self.plot1.addItem(gx)
+        # self.plot1.addItem(gy)
+        # self.plot1.addItem(gz)
+        # self.plot.showMaximized()
+        # pts = np.vstack(0, 0 ,0).transpose()
+        # traces= gl.GLLinePlotItem(pos=pts)
+        # self.plot.addItem(traces)
         v0_xArray = []
         v0_yArray = []
         v0_zArray = []
-        #self.plot.resizeGL(400,400)
+        # self.plot.resizeGL(400,400)
         plot_xyz = np.vstack([v0_xArray, v0_yArray, v0_zArray]).transpose()
         # self.curve = self.plot.plot.getPlotItem().plot()
         self.traces = gl.GLLinePlotItem(pos=plot_xyz)
@@ -233,3 +262,4 @@ class OL_3D_Plot(QtGui.QWidget):
 
         layout.addWidget(self.plot)
         self.setLayout(layout)
+
--- Object_Localization.py
+++ Object_Localization.py
@@ -1,12 +1,15 @@
 import argparse
 
-
 import cv2
 import imutils
 import numpy as np
 
 
 def Object_Localization(frame, newPts, newCounter):
+    isGreenDetected = False
+    isRedDetected = False
+    isBlueDetected = False
+    isYellowDetected = False
 
     def distance_to_camera(knownWidth, focalLength, perWidth):
         # compute and return the distance from the image to camera
@@ -27,29 +30,28 @@ def Object_Localization(frame, newPts, newCounter):
     # define the lower and upper boundaries of the "green"
     # ball in the HSV color space, then initialize the
     # list of tracked points
-    #greenLower =  (29, 86, 6)
-    #greenUpper = (64, 255, 255)
 
-    lower = {'red': (166, 84, 141), 'green': (66, 122, 129), 'blue': (97, 100, 117), 'yellow': (23, 59, 119),
-             'orange': (0, 50, 80)}  # assign new item lower['blue'] = (93, 10, 0)
-    upper = {'red': (186, 255, 255), 'green': (86, 255, 255), 'blue': (117, 255, 255), 'yellow': (54, 255, 255),
-             'orange': (20, 255, 255)}
+    lower = {'yellow': (23, 59, 119), 'green': (66, 122, 129), 'blue': (97, 100, 117),
+             'red': (166, 84, 141)}  # assign new item lower['blue'] = (93, 10, 0)
+    upper = {'yellow': (54, 255, 255), 'green': (86, 255, 255), 'blue': (117, 255, 255), 'red': (186, 255, 255)}
 
     # define standard colors for circle around the object
-    colors = {'red': (0, 0, 255), 'green': (0, 255, 0), 'blue': (255, 0, 0), 'yellow': (0, 255, 217),
-              'orange': (0, 140, 255)}
+    colors = {'red': (0, 0, 255), 'green': (0, 255, 0), 'blue': (255, 0, 0), 'yellow': (0, 255, 217)}
 
     pts = newPts
     counter = newCounter
     inches = 0
     (x, y, z) = (0, 0, 0)
 
-    #fig = plt.figure()
-    #ax = fig.gca(projection='3d')
-    #ax.set_xlabel('X-Direction')
-    #ax.set_ylabel('Y-Direction')
-    #ax.set_zlabel('Z-Direction')
+    (x_red, x_green, x_blue, x_yellow) = (0, 0, 0, 0)
+    (y_red, y_green, y_blue, y_yellow) = (0, 0, 0, 0)
+    (z_red, z_green, z_blue, z_yellow) = (0, 0, 0, 0)
 
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+    # ax.set_xlabel('X-Direction')
+    # ax.set_ylabel('Y-Direction')
+    # ax.set_zlabel('Z-Direction')
 
     # resize the frame, blur it, and convert it to the HSV
     # color space
@@ -59,69 +61,243 @@ def Object_Localization(frame, newPts, newCounter):
 
     for key, value in upper.items():
 
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
-       # mask = cv2.inRange(hsv, greenLower, greenUpper)
-        kernel = np.ones((9, 9), np.uint8)
-        mask = cv2.inRange(hsv, lower[key], upper[key])
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-        #mask = cv2.erode(mask, None, iterations=2)
-        #mask = cv2.dilate(mask, None, iterations=2)
+        if (key == "red"):
+            # construct a mask for the color "green", then perform
+            # a series of dilations and erosions to remove any small
+            # blobs left in the mask
+            # mask = cv2.inRange(hsv, greenLower, greenUpper)
+            kernel = np.ones((9, 9), np.uint8)
+            mask = cv2.inRange(hsv, lower[key], upper[key])
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+            # mask = cv2.erode(mask, None, iterations=2)
+            # mask = cv2.dilate(mask, None, iterations=2)
 
-        # find contours in the mask
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+            # find contours in the mask
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-        # and initialize center of the ball
-        center = None
-        isDetected = False
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            # and initialize center of the ball
+            center = None
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                # find the largest contour in the mask, then use
+                # it to compute the minimum enclosing circle and
+                # centroid
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-            # Get distance for Z-axis using reference image (In inches)
-            marker = cv2.minAreaRect(c)
-            inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+                # Get distance for Z-axis using reference image (In inches)
+                marker = cv2.minAreaRect(c)
+                inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
 
-            # only proceed if the radius meets a minimum size
-            if radius > 10:
-                # draw the circle and centroid on the frame
-                #cv2.circle(thisFrame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-                cv2.circle(thisFrame, (int(x), int(y)), int(radius), colors[key], 2)
-                cv2.circle(thisFrame, center, 5, (0, 0, 255), -1)
-                pts.appendleft(center)
-                isDetected = True
-            else:
-                isDetected = False
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    # draw the circle and centroid on the frame
+                    # cv2.circle(thisFrame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(thisFrame, (int(x), int(y)), int(radius), colors[key], 2)
+                    cv2.circle(thisFrame, center, 5, (0, 0, 255), -1)
+                    pts.appendleft(center)
+                    isRedDetected = True
 
+                else:
+                    isRedDetected = False
 
+        if (key == "yellow"):
+            # construct a mask for the color "green", then perform
+            # a series of dilations and erosions to remove any small
+            # blobs left in the mask
+            # mask = cv2.inRange(hsv, greenLower, greenUpper)
+            kernel = np.ones((9, 9), np.uint8)
+            mask = cv2.inRange(hsv, lower[key], upper[key])
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+            # mask = cv2.erode(mask, None, iterations=2)
+            # mask = cv2.dilate(mask, None, iterations=2)
+
+            # find contours in the mask
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+            # and initialize center of the ball
+            center = None
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                # find the largest contour in the mask, then use
+                # it to compute the minimum enclosing circle and
+                # centroid
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                # Get distance for Z-axis using reference image (In inches)
+                marker = cv2.minAreaRect(c)
+                inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    # draw the circle and centroid on the frame
+                    # cv2.circle(thisFrame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(thisFrame, (int(x), int(y)), int(radius), colors[key], 2)
+                    cv2.circle(thisFrame, center, 5, (0, 0, 255), -1)
+                    pts.appendleft(center)
+                    isYellowDetected = True
+
+                else:
+                    isYellowDetected = False
+
+        if (key == "blue"):
+            # construct a mask for the color "green", then perform
+            # a series of dilations and erosions to remove any small
+            # blobs left in the mask
+            # mask = cv2.inRange(hsv, greenLower, greenUpper)
+            kernel = np.ones((9, 9), np.uint8)
+            mask = cv2.inRange(hsv, lower[key], upper[key])
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+            # mask = cv2.erode(mask, None, iterations=2)
+            # mask = cv2.dilate(mask, None, iterations=2)
+
+            # find contours in the mask
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+            # and initialize center of the ball
+            center = None
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                # find the largest contour in the mask, then use
+                # it to compute the minimum enclosing circle and
+                # centroid
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                # Get distance for Z-axis using reference image (In inches)
+                marker = cv2.minAreaRect(c)
+                inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    # draw the circle and centroid on the frame
+                    # cv2.circle(thisFrame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(thisFrame, (int(x), int(y)), int(radius), colors[key], 2)
+                    cv2.circle(thisFrame, center, 5, (0, 0, 255), -1)
+                    pts.appendleft(center)
+                    isBlueDetected = True
+
+                else:
+                    isBlueDetected = False
+
+        if (key == "green"):
+            # construct a mask for the color "green", then perform
+            # a series of dilations and erosions to remove any small
+            # blobs left in the mask
+            # mask = cv2.inRange(hsv, greenLower, greenUpper)
+            kernel = np.ones((9, 9), np.uint8)
+            mask = cv2.inRange(hsv, lower[key], upper[key])
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+            # mask = cv2.erode(mask, None, iterations=2)
+            # mask = cv2.dilate(mask, None, iterations=2)
+
+            # find contours in the mask
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+            # and initialize center of the ball
+            center = None
+            # only proceed if at least one contour was found
+            if len(cnts) > 0:
+                # find the largest contour in the mask, then use
+                # it to compute the minimum enclosing circle and
+                # centroid
+                c = max(cnts, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                # Get distance for Z-axis using reference image (In inches)
+                marker = cv2.minAreaRect(c)
+                inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+                # only proceed if the radius meets a minimum size
+                if radius > 10:
+                    # draw the circle and centroid on the frame
+                    # cv2.circle(thisFrame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+                    cv2.circle(thisFrame, (int(x), int(y)), int(radius), colors[key], 2)
+                    cv2.circle(thisFrame, center, 5, (0, 0, 255), -1)
+                    pts.appendleft(center)
+                    isGreenDetected = True
+
+                else:
+                    isGreenDetected = False
+
+        print("\n" + str(key) + " pts length:\t" + str(len(pts)))
         for i in np.arange(1, len(pts)):
             # if either of the tracked points are None, ignore
             if pts[i - 1] is None or pts[i] is None:
+                print(" Not Enough Points ")
                 continue
 
             # check to see if enough points have been accumulated in
             # the buffer
             if counter >= 10 and i == 1 and pts[-10] is not None:
                 # COMPUTE POINTS AND STORE INTO DATA STRUCTURE
-                x = pts[-10][0] - pts[i][0]
-                y = pts[-10][1] - pts[i][1]
-                z = round(inches)
+                if isRedDetected:
+                    print(" Red Detected ")
+                    x_red = pts[-10][0] - pts[i][0]
+                    y_red = pts[-10][1] - pts[i][1]
+                    z_red = round(inches)
+                if isGreenDetected:
+                    print(" Green Detected ")
+                    x_green = pts[-10][0] - pts[i][0]
+                    y_green = pts[-10][1] - pts[i][1]
+                    z_green = round(inches)
+
+                if isBlueDetected:
+                    print(" Blue Detected ")
+                    x_blue = pts[-10][0] - pts[i][0]
+                    y_blue = pts[-10][1] - pts[i][1]
+                    z_blue = round(inches)
+
+                if isYellowDetected:
+                    print(" Yellow Detected ")
+                    x_yellow = pts[-10][0] - pts[i][0]
+                    y_yellow = pts[-10][1] - pts[i][1]
+                    z_yellow = round(inches)
+
+                # x = pts[-10][0] - pts[i][0]
+                # y = pts[-10][1] - pts[i][1]
+                # z = round(inches)
 
                 # draw the connecting lines
                 thickness = int(np.sqrt(args["buffer"] / float(i + 1)))
                 cv2.line(thisFrame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-
     # return the frame and increment counter
     counter += 1
-    #print("counter: " + str(counter) + "\tpts: " + str(len(pts)) + " \t\t\tx: " + str(x) + "\t\t\ty: " + str(y) + "\t\t\tz: " + str(z))
-    return thisFrame, x, y, z, pts, counter, isDetected
+
+    print("----------------------------------------------------------------------------------------------")
+    print("COUNTER: " + str(counter) +
+          "\t\t\nRED:\t" + str(isRedDetected) + " \t\tx_red:\t\t" + str(x_red) + "\t\ty_red:\t\t" + str(
+        y_red) + "\t\tz_red:\t\t" + str(z_red) +
+          "\t\t\nGREEN:\t" + str(isGreenDetected) + " \t\tx_green:\t" + str(x_green) + "\t\ty_green:\t" + str(
+        y_green) + "\t\tz_green:\t" + str(z_green) +
+          "\t\t\nBLUE:\t" + str(isBlueDetected) + " \t\tx_blue:\t\t" + str(x_blue) + "\t\ty_blue:\t\t" + str(
+        y_blue) + "\t\tz_blue:\t\t" + str(z_blue) +
+          "\t\t\nYELLOW:\t" + str(isYellowDetected) + " \t\tx_yellow:\t" + str(x_yellow) + "\t\ty_yellow:\t" + str(
+        y_yellow) + "\t\tz_yellow:\t" + str(z_yellow)
+          )
+
+    # print("counter: " + str(counter) + "\tpts: " + str(len(pts)) + " \t\t\tx: " + str(x) + "\t\t\ty: " + str(y) + "\t\t\tz: " + str(z))
+    return thisFrame, \
+           x_red, y_red, z_red, \
+           x_green, y_green, z_green, \
+           x_blue, y_blue, z_blue, \
+           x_yellow, y_yellow, z_yellow, \
+           pts, counter, isRedDetected, isGreenDetected, isBlueDetected, isYellowDetected
